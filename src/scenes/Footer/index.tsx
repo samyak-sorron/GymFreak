6d7545d33df27bf5addc1bbfd0ddc0b156aa4fa1
samyak <samyak.sharma.758@gmail.com>
2024-08-12 09:59:54 +0530

    all done

--- src/scenes/Footer/index.tsx
+++ src/scenes/Footer/index.tsx
@@ -1,8 +1,6 @@
 import Logo from "@/assets/assets/Logo.png"
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
         <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
